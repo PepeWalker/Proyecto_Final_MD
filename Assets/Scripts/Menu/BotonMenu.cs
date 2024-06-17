2cfffafe1f85e57c23f04b661e5835a6680aab85
Jorge Palma <jorge_dibus@hotmail.com>
2024-06-17 18:07:13 +0200

    Arreglo de botones para quitar redundancias
    Quitados tres scrips redundantes, juntados los scrips de boton jugar etc en BotonMenu en el objeto canvas, y la acciones de aumentar, reproducir sonido, poner underline, las he separado por funciones si ntner que reescribir scrips y todo junto en un sitio.

--- Assets/Scripts/Menu/BotonMenu.cs
+++ Assets/Scripts/Menu/BotonMenu.cs
@@ -6,54 +6,70 @@ using UnityEngine.EventSystems;
 
 using UnityEngine.UI;
 
-public class BotonMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class BotonMenu : MonoBehaviour
 {
-    
-        // Start is called before the first frame update
 
-    public TextMeshProUGUI buttonText;
+    // Start is called before the first frame update
+
     public AudioSource audioSource;
     public AudioClip strikeOutSound;
     public AudioClip hoverSound; //Sonido de lapiz rayado del texto
     public AudioClip changeSceneSound; //sonido de paginas para cambio de escena o nose  
-    public GameObject textureObject; //textura de tachado
 
-    void Start()
+
+
+
+    public void OnEnterIncreaseSizeText(TextMeshProUGUI t)
     {
-        //la textura tachado debe estar desactivada hasta que se seleccione el boton
-        if (textureObject != null)
+        if (t != null)
         {
-            textureObject.SetActive(false);
+            t.fontSize += 3;
         }
         
     }
 
-
-    public virtual void OnPointerEnter(PointerEventData eventData)
+    public void OnEnterUnderlineText(TextMeshProUGUI t)
     {
-        if (buttonText != null)
+        if (t != null)
         {
-            buttonText.fontSize += 3;
+            t.fontStyle = FontStyles.Underline;
         }
-        if (audioSource != null && hoverSound != null)
+
+    }
+
+    
+    public void OnEnterPlaySound(AudioClip ac) 
+    {
+        //por ahora pongo sonido predeterminado, quizas lo cambio a una lsita de sonidos y que coja uno aleatorio
+        // quiero que si hay un sonido ya reproduciendo, en vez de no reproducir, que lo reproduzca con menos volumen
+        if (audioSource != null && hoverSound != null && !audioSource.isPlaying)
         {
             audioSource.PlayOneShot(hoverSound);
         }
     }
 
-    public virtual void OnPointerExit(PointerEventData eventData)
+
+    public virtual void OnExitReduceSizeText(TextMeshProUGUI t)
     {
-        if (buttonText != null)
+        if (t != null)
         {
-            buttonText.fontSize -= 3;
+            t.fontSize -= 3;
         }
     }
 
-    public virtual void OnPointerClick(PointerEventData eventData)
+    public virtual void OnExitNormalStyleText(TextMeshProUGUI t)
     {
-        if (textureObject != null)
+        if (t != null)
         {
-            textureObject.SetActive(true); //No funciona, pilla la textura del textmeshpro, nose
+            t.fontStyle = FontStyles.Normal;
+        }
+    }
+
+    public virtual void OnClick(GameObject g)
+    {
+        if (g != null)
+        {
+            g.SetActive(true); //No funciona, pilla la textura del textmeshpro, nose
             audioSource.PlayOneShot(strikeOutSound);
         }
     }
